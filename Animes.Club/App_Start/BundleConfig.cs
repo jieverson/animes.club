6b8ee6a816e585b246d19f5d206a93a4c387100f
Jieverson Maissiat <jieverson@craftbox.com.br>
2015-01-10 15:35:52 -0200

    Trying to put a theme

--- Animes.Club/App_Start/BundleConfig.cs
+++ Animes.Club/App_Start/BundleConfig.cs
@@ -7,12 +7,12 @@ namespace Animes.Club
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/css/bootstrap.css",
                  "~/css/font-awesome.css",
+                 //"~/css/animes.club.theme.css",
                  "~/css/animes.club.css"
                  ));
 
@@ -43,32 +43,8 @@ namespace Animes.Club
                 "~/scripts/handlebars-v2.0.0.js"
                 ));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //    "~/Scripts/jquery.unobtrusive*",
-            //    "~/Scripts/jquery.validate*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-            //    "~/Scripts/knockout-{version}.js",
-            //    "~/Scripts/knockout.validation.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/app").Include(
-            //    "~/Scripts/sammy-{version}.js",
-            //    "~/Scripts/app/common.js",
-            //    "~/Scripts/app/app.datamodel.js",
-            //    "~/Scripts/app/app.viewmodel.js",
-            //    "~/Scripts/app/home.viewmodel.js",
-            //    "~/Scripts/app/_run.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             //    "~/Scripts/modernizr-*"));
-
-            
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            //BundleTable.EnableOptimizations = true;
         }
     }
 }
