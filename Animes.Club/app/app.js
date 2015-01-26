3069f0eb6afa276ee616a70a1c032f393869b977
Jieverson Maissiat <jieverson@craftbox.com.br>
2015-01-26 04:34:51 -0200

    Terms

--- Animes.Club/app/app.js
+++ Animes.Club/app/app.js
@@ -14,6 +14,7 @@
         $routeProvider.when("/dropped", { templateUrl: "/view/anime/list", controller: "MyLists", status: 4 });
         $routeProvider.when("/anime/:id/:name", { templateUrl: "/view/anime/profile", controller: "Anime" });
         $routeProvider.when("/user/:username", { templateUrl: "/view/user/profile", controller: "User" });
+        $routeProvider.when("/terms", { templateUrl: "/view/login/terms" });
 
         //Handle
         //$routeProvider.otherwise({
